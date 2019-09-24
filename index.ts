0bd95be1dbc894ef49d77248ab163caeb6feed6f
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-09-24 14:53:58 +0200

    added basic jolocomlib boilerplate

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
 import * as SP from 'serialport';
+import { JolocomLib } from "jolocom-lib"
 
 const portAddrs = process.env.PORT || "/dev/ttyACM0"
+
+const seed = "a".repeat(64)
+const pword = "b".repeat(64)
+
+const vkp = JolocomLib.KeyProvider.fromSeed(Buffer.from(seed, 'hex'), pword)
+
+const idw = JolocomLib.registries.jolocom.create().authenticate(vkp, {
+    derivationPath: JolocomLib.KeyTypes.jolocomIdentityKey,
+    encryptionPass: pword
+})
 
 const port = new SP(portAddrs, {
     baudRate:115200
@@ -15,7 +26,7 @@ const parser = new SP.parsers.Readline({
 port.pipe(parser)
 
 parser.on("data", data => {
-    console.log(data)
+    console.log(`recieved: ${data}`)
     // send back new one and validate/act on recieved
 })
 
