361f012ac5911ad8fe119e0e3cc4f2524bb5bcdc
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-10-23 08:40:15 +0200

    types and formatting

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import * as SP from 'serialport';
 import { JolocomLib } from "jolocom-lib"
+import { IdentityWallet } from 'jolocom-lib/js/identityWallet/identityWallet';
+import { JSONWebToken } from 'jolocom-lib/js/interactionTokens/JSONWebToken';
+import { CredentialResponse } from 'jolocom-lib/js/interactionTokens/credentialResponse';
 
 const seed = "a".repeat(64)
 const pword = "b".repeat(64)
@@ -7,22 +10,22 @@ const pword = "b".repeat(64)
 const vkp = JolocomLib.KeyProvider.fromSeed(Buffer.from(seed, 'hex'), pword)
 
 const setupPort = (port: SP.PortInfo) => new SP(port.comName, {
-    baudRate: 1000000,
-    autoOpen: false,
-    rtscts: true
+  baudRate: 1000000,
+  autoOpen: false,
+  rtscts: true
 }, err => err ? console.error(err.toString()) : null)
 
 const configPort = (sp: SP) => (cb: (line: string) => void) => sp.pipe(new SP.parsers.Readline({
-    delimiter: '\n',
-    encoding: 'ascii',
-    includeDelimiter: false
+  delimiter: '\n',
+  encoding: 'ascii',
+  includeDelimiter: false
 })).on("data", cb)
 
 const openPort = (sp: SP) => (initialWrite: string) => sp.open(err => sp.write(initialWrite) && err ? console.error(err.toString()) : null)
 
 JolocomLib.registries.jolocom.create().authenticate(vkp, {
-    derivationPath: JolocomLib.KeyTypes.jolocomIdentityKey,
-    encryptionPass: pword
+  derivationPath: JolocomLib.KeyTypes.jolocomIdentityKey,
+  encryptionPass: pword
 }).then(async (idw) => {
     console.log("id created")
 
