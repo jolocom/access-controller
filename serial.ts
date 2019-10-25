2c508ab578288ac93c5a4dfdf44b36d487c37dc1
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-10-25 22:48:28 +0200

    fix null bug

--- serial.ts
+++ serial.ts
@@ -25,7 +25,7 @@ export const streamValidator = (validate: (jwt: string) => Promise<boolean>) =>
 
 export const printer = new Writable({
     write: (chunk, encoding, cb) => {
-        console.log(chunk.toString())
+        console.log(chunk && chunk.toString())
         cb()
     }
 })
