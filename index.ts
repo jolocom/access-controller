6cc055d6ea63e8f9954871a6de527d9560446cce
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-11-09 23:55:13 +0100

    use cred req

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
 import * as SP from 'serialport';
 import { streamValidator } from './serial'
 import { JolocomLib } from "jolocom-lib"
+import { constraintFunctions } from 'jolocom-lib/js/interactionTokens/credentialRequest'
 
 const seed = "a".repeat(64)
 const pword = "b".repeat(64)
 
 const vkp = JolocomLib.KeyProvider.fromSeed(Buffer.from(seed, 'hex'), pword)
+
+const credReqAttrs = (callback: string, issuer: string) => ({
+    callbackURL: callback,
+    credentialRequirements: [{
+        type: ['accessKey'],
+        constraints: [constraintFunctions.is('issuer', issuer)]
+    }]
+})
 
 const setupPort = (port: string) => new SP(port, {
   baudRate: 115200,
@@ -31,17 +40,18 @@ JolocomLib.registries.jolocom.create().authenticate(vkp, {
     valid
       ? console.log("valid")
       : console.log("invalid")
-    port.write(await idw.create.interactionTokens.request.auth({
-      callbackURL: 'new'
-    }, pword).then(t => t.encode() + '\n'))
+    port.write(await idw.create.interactionTokens.request.share(
+        credReqAttrs('https://google.com', 'did'),
+        pword
+    ).then(t => t.encode() + '\n'))
   }))
 
   port.open(async err => {
     console.error(err)
-    port.write(await idw.create.interactionTokens.request.auth({
-      callbackURL: 'ble lol',
-      description: 'ble lol'
-    }, pword).then(t => t.encode() + '\n'))
+    port.write(await idw.create.interactionTokens.request.share(
+        credReqAttrs('https://google.com', 'did'),
+        pword
+    ).then(t => t.encode() + '\n'))
   })
 
 }).catch(console.error)
