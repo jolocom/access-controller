d280e14e77a8c8abe42f8fb3833446f4cd56f057
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-11-13 12:52:59 +0100

    improve documentation and clarify validation logs

--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@ const pword = "b".repeat(64)
 
 const vkp = JolocomLib.KeyProvider.fromSeed(Buffer.from(seed, 'hex'), pword)
 
+// Credential request definition
 const credReqAttrs = (callback: string, issuer: string) => ({
   callbackURL: callback,
   credentialRequirements: [{
@@ -30,44 +31,62 @@ JolocomLib.registries.jolocom.create().authenticate(vkp, {
   encryptionPass: pword
 }).then(async (idw) => {
 
+  // mapping of door IDs to serial ports
   const doorPorts = {
     '1': '/dev/ttyACM0',
     '4': '/dev/ttyACM1'
   }
 
+  // for each doorID, open the corrosponding serial port
   Object.keys(doorPorts)
     .map(door => {
       const port = setupPort(doorPorts[door])
+
+      // pipe the output of the port through the stream validator
       port.pipe(streamValidator((jwt: string) => {
+
+        // parse the recieved token and ensure it's a credential response
         const token = JolocomLib.parse.interactionToken.fromJWT<CredentialResponse>(jwt)
+
         if (token.interactionType !== InteractionType.CredentialResponse)
           return Promise.resolve(false)
+
+        // ensure the response contains an accessKey credential
         const accessCred = token.interactionToken.suppliedCredentials.find(c => c.type.includes('AccessKey', 1))
+
         if (!accessCred || !accessCred.claim || !accessCred.claim.token)
           return Promise.resolve(false)
+
         try {
+          // test for doorID membership in the accessKey credential
           const access: string[] = (accessCred.claim.token as string).split(',')
-          console.log(access)
+
           if (!access.includes(door))
             return Promise.resolve(false)
+
+          // return validation result
           return JolocomLib.util.validateDigestable(token)
         } catch (err) {
           return Promise.resolve(false)
         }
       })(async valid => {
+        // act on the validity of the token recieved
         if (valid) {
-          console.log("valid")
+          console.log(`Door ${door} valid`)
         } else {
-          console.log("invalid")
+          console.log(`Door ${door} invalid`)
         }
+
+        // write a new request token to the relayer
         port.write(await idw.create.interactionTokens.request.share(
           credReqAttrs('ble', 'did'),
           pword
         ).then(t => t.encode() + '\n'))
       }))
 
+      // write the initial request token on port opening
       port.open(async err => {
-        console.error(err)
+        if (err) console.error(err)
         port.write(await idw.create.interactionTokens.request.share(
           credReqAttrs('ble', 'did'),
           pword
