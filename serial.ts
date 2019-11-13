b76dd36a9b3c0d9a12e0f3ee9a5f01196d010eba
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-11-13 12:54:51 +0100

    add a description of the streamValidator

--- serial.ts
+++ serial.ts
@@ -16,6 +16,9 @@ export const openPort = (baud: number, path: string) => {
 
 const delimit = options => new parsers.Readline(options)
 
+// streamValidator breaks up a stream by newline characters, runs the validate
+// function on the delimited string and then runts the validityCallback function
+// on the result of the validate function
 export const streamValidator = (validate: (jwt: string) => Promise<boolean>) =>
     (validityCallback: (validity: boolean) => void) => delimit({
         delimiter: '\n',
