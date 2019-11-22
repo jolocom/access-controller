fe0c5f6d1913b97f3c6614a7f1ec130389caa4a6
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-11-22 15:05:28 +0100

    testing

--- leds.ts
+++ leds.ts
@@ -1,7 +1,14 @@
 import { Gpio } from 'onoff'
 
-const validLED = new Gpio(16, 'out')
-const invalidLED = new Gpio(2, 'out')
+// const validLED = new Gpio(16, 'out')
+// const invalidLED = new Gpio(2, 'out')
+
+let r = (s, e) => Array.from('x'.repeat(e - s), (_, i) => s + i);
+
+const leds = r(0, 50).map(n => new Gpio(n, 'out'))
+
+leds.map(led => led.write(1))
+setTimeout(_ => leds.map(led => led.write(0)), 1000)
 
 const blinkRepeat = led => time => n => {
   // turn led on
@@ -14,8 +21,6 @@ const blinkRepeat = led => time => n => {
   }, time)
 }
 
-export const showSuccess = () => blinkRepeat(validLED)(500)(3)
-export const showFailure = () => blinkRepeat(invalidLED)(500)(3)
+// export const showSuccess = () => blinkRepeat(validLED)(500)(3)
+// export const showFailure = () => blinkRepeat(invalidLED)(500)(3)
 
-showSuccess()
-showFailure()
